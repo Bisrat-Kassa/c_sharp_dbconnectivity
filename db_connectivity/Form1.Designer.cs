c0c7fec1051a8b1d9ed95d54c77f9cea4c74b1f7
Bisrat-Kassa <107492307+Bisrat-Kassa@users.noreply.github.com>
2022-08-28 18:13:15 -0700

    first commit
    delete query not implemented

---
+++ db_connectivity/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace db_connectivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_variable = new System.Windows.Forms.RadioButton();
+            this.simple_rb = new System.Windows.Forms.RadioButton();
+            this.txt_Price = new System.Windows.Forms.TextBox();
+            this.txt_Quantity = new System.Windows.Forms.TextBox();
+            this.txt_Item = new System.Windows.Forms.TextBox();
+            this.txt_uname = new System.Windows.Forms.TextBox();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.lbl_Price = new System.Windows.Forms.Label();
+            this.lbl_Quantity = new System.Windows.Forms.Label();
+            this.lbl_Item_name = new System.Windows.Forms.Label();
+            this.lbl_Date = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.refresh_btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 29);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Username";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_variable);
+            this.groupBox1.Controls.Add(this.simple_rb);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(496, 194);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(336, 78);
+            this.groupBox1.TabIndex = 37;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Product type";
+            // 
+            // rb_variable
+            // 
+            this.rb_variable.AutoSize = true;
+            this.rb_variable.Location = new System.Drawing.Point(164, 29);
+            this.rb_variable.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rb_variable.Name = "rb_variable";
+            this.rb_variable.Size = new System.Drawing.Size(117, 29);
+            this.rb_variable.TabIndex = 18;
+            this.rb_variable.TabStop = true;
+            this.rb_variable.Text = "Variable";
+            this.rb_variable.UseVisualStyleBackColor = true;
+            // 
+            // simple_rb
+            // 
+            this.simple_rb.AutoSize = true;
+            this.simple_rb.Location = new System.Drawing.Point(8, 33);
+            this.simple_rb.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.simple_rb.Name = "simple_rb";
+            this.simple_rb.Size = new System.Drawing.Size(99, 29);
+            this.simple_rb.TabIndex = 17;
+            this.simple_rb.TabStop = true;
+            this.simple_rb.Text = "simple";
+            this.simple_rb.UseVisualStyleBackColor = true;
+            // 
+            // txt_Price
+            // 
+            this.txt_Price.Location = new System.Drawing.Point(240, 228);
+            this.txt_Price.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_Price.Name = "txt_Price";
+            this.txt_Price.Size = new System.Drawing.Size(148, 26);
+            this.txt_Price.TabIndex = 34;
+            // 
+            // txt_Quantity
+            // 
+            this.txt_Quantity.Location = new System.Drawing.Point(33, 228);
+            this.txt_Quantity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_Quantity.Name = "txt_Quantity";
+            this.txt_Quantity.Size = new System.Drawing.Size(148, 26);
+            this.txt_Quantity.TabIndex = 33;
+            // 
+            // txt_Item
+            // 
+            this.txt_Item.Location = new System.Drawing.Point(33, 138);
+            this.txt_Item.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_Item.Name = "txt_Item";
+            this.txt_Item.Size = new System.Drawing.Size(786, 26);
+            this.txt_Item.TabIndex = 32;
+            // 
+            // txt_uname
+            // 
+            this.txt_uname.Location = new System.Drawing.Point(33, 53);
+            this.txt_uname.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_uname.Name = "txt_uname";
+            this.txt_uname.Size = new System.Drawing.Size(224, 26);
+            this.txt_uname.TabIndex = 31;
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cancel.Location = new System.Drawing.Point(363, 301);
+            this.btn_Cancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(112, 49);
+            this.btn_Cancel.TabIndex = 28;
+            this.btn_Cancel.Text = "update";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.Location = new System.Drawing.Point(136, 301);
+            this.btn_Add.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(112, 49);
+            this.btn_Add.TabIndex = 27;
+            this.btn_Add.Text = "Insert";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // lbl_Price
+            // 
+            this.lbl_Price.AutoSize = true;
+            this.lbl_Price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Price.Location = new System.Drawing.Point(235, 194);
+            this.lbl_Price.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Price.Name = "lbl_Price";
+            this.lbl_Price.Size = new System.Drawing.Size(74, 29);
+            this.lbl_Price.TabIndex = 26;
+            this.lbl_Price.Text = "Price";
+            // 
+            // lbl_Quantity
+            // 
+            this.lbl_Quantity.AutoSize = true;
+            this.lbl_Quantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Quantity.Location = new System.Drawing.Point(28, 194);
+            this.lbl_Quantity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Quantity.Name = "lbl_Quantity";
+            this.lbl_Quantity.Size = new System.Drawing.Size(108, 29);
+            this.lbl_Quantity.TabIndex = 25;
+            this.lbl_Quantity.Text = "Quantity";
+            // 
+            // lbl_Item_name
+            // 
+            this.lbl_Item_name.AutoSize = true;
+            this.lbl_Item_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Item_name.Location = new System.Drawing.Point(28, 104);
+            this.lbl_Item_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Item_name.Name = "lbl_Item_name";
+            this.lbl_Item_name.Size = new System.Drawing.Size(139, 29);
+            this.lbl_Item_name.TabIndex = 24;
+            this.lbl_Item_name.Text = "Item Name";
+            // 
+            // lbl_Date
+            // 
+            this.lbl_Date.AutoSize = true;
+            this.lbl_Date.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Date.Location = new System.Drawing.Point(315, 19);
+            this.lbl_Date.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Date.Name = "lbl_Date";
+            this.lbl_Date.Size = new System.Drawing.Size(67, 29);
+            this.lbl_Date.TabIndex = 22;
+            this.lbl_Date.Text = "Date";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(579, 301);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 49);
+            this.button1.TabIndex = 40;
+            this.button1.Text = "delete";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(319, 51);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 41;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 381);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1104, 287);
+            this.dataGridView1.TabIndex = 42;
+            // 
+            // refresh_btn
+            // 
+            this.refresh_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refresh_btn.Location = new System.Drawing.Point(774, 301);
+            this.refresh_btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.refresh_btn.Name = "refresh_btn";
+            this.refresh_btn.Size = new System.Drawing.Size(144, 49);
+            this.refresh_btn.TabIndex = 43;
+            this.refresh_btn.Text = "View";
+            this.refresh_btn.UseVisualStyleBackColor = true;
+            this.refresh_btn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1290, 696);
+            this.Controls.Add(this.refresh_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txt_Price);
+            this.Controls.Add(this.txt_Quantity);
+            this.Controls.Add(this.txt_Item);
+            this.Controls.Add(this.txt_uname);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.lbl_Price);
+            this.Controls.Add(this.lbl_Quantity);
+            this.Controls.Add(this.lbl_Item_name);
+            this.Controls.Add(this.lbl_Date);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_variable;
+        private System.Windows.Forms.RadioButton simple_rb;
+        private System.Windows.Forms.TextBox txt_Price;
+        private System.Windows.Forms.TextBox txt_Quantity;
+        private System.Windows.Forms.TextBox txt_Item;
+        private System.Windows.Forms.TextBox txt_uname;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Label lbl_Price;
+        private System.Windows.Forms.Label lbl_Quantity;
+        private System.Windows.Forms.Label lbl_Item_name;
+        private System.Windows.Forms.Label lbl_Date;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button refresh_btn;
+    }
+}
+
---
+++ db_connectivity/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace db_connectivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=LAPTOP-NCEDVHBS;Initial Catalog=c_sharpdb;Integrated Security=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into info values(@uname , @reg_date, @iname, @quant , @price,@p_type) ", con);
+                cmd.Parameters.AddWithValue("@uname", txt_uname.Text);
+                cmd.Parameters.AddWithValue("@reg_date", dateTimePicker1.Text);
+                cmd.Parameters.AddWithValue("@iname", txt_Item.Text);
+                cmd.Parameters.AddWithValue("@quant", int.Parse(txt_Quantity.Text));
+                cmd.Parameters.AddWithValue("@price", float.Parse(txt_Price.Text));
+                if (simple_rb.Checked == true)
+                {
+                    cmd.Parameters.AddWithValue("@p_type", simple_rb.Text);
+                }
+                else if (rb_variable.Checked == true)
+                {
+                    cmd.Parameters.AddWithValue("@p_type", rb_variable.Text);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@p_type", "Unknown");
+                }
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("New item inserted");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("connection not established!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-NCEDVHBS;Initial Catalog=c_sharpdb;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from info",  con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-NCEDVHBS;Initial Catalog=c_sharpdb;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = 
+                new SqlCommand("update info set uname= @uname,reg_date= @reg_date, iname = @iname, quant = @quant, price = @price, p_type = @p_type where iname = @iname ", con);
+            cmd.Parameters.AddWithValue("@uname", txt_uname.Text);
+            cmd.Parameters.AddWithValue("@reg_date", dateTimePicker1.Text);
+            cmd.Parameters.AddWithValue("@iname", txt_Item.Text);
+            cmd.Parameters.AddWithValue("@quant", int.Parse(txt_Quantity.Text));
+            cmd.Parameters.AddWithValue("@price", float.Parse(txt_Price.Text));
+            if (simple_rb.Checked == true)
+            {
+                cmd.Parameters.AddWithValue("@p_type", simple_rb.Text);
+            }
+            else if (rb_variable.Checked == true)
+            {
+                cmd.Parameters.AddWithValue("@p_type", rb_variable.Text);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@p_type", "Unknown");
+            }
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Item Updated");
+        }
+    }
+}
